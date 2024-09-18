9db13556b4713b2228f17553b03c8340e427cca7
Maxim Pachuzheuski <makspohp123@gmail.com>
2024-09-18 19:22:59 +0300

    feat:finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.currentNumber = Math.ceil((this.min + (this.max - this.min)/2));
+        return this.currentNumber
     }
 
     lower() {
-
+        this.max = this.currentNumber;
     }
 
     greater() {
-
+        this.min = this.currentNumber;
     }
 }
 
